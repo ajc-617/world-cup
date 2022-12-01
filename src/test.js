059e28c4ac0ba997dd1e92203f43eaffad29001e
ajc-617 <43360221+ajc-617@users.noreply.github.com>
2022-12-01 17:28:18 -0500

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,13 +1,38 @@
-const request = require('request');
+const request = require('request')
+const readline = require('readline');
 
 
 //Senegal vs Netherlands is fixture id # 855734
 //league id 39 for premier league
 
+function oddsToPercent(odds) {
+  if (odds < 0) {
+    return (odds*-1) / (odds*-1 + 100)
+  }
+  else {
+    return 100 / (odds + 100)
+  }
+}
+
+let teams = []
+
+process.argv.forEach(function (val, index, array) {
+  if (index > 1) {
+    teams.push(val)
+  }
+});
+
+console.log(teams)
+process.exit(0)
+
 let options = {
   method: 'GET',
-  url: 'https://api.the-odds-api.com/v4/sports',
-  qs: {apiKey: '91c553bdf4ea2ce831d77831c7cb55a5'},
+  url: 'https://api.the-odds-api.com/v4/sports/soccer_epl/odds',
+  qs: {
+    apiKey: '91c553bdf4ea2ce831d77831c7cb55a5',
+    regions: 'us', 
+    oddsFormat: 'american'
+  },
   headers: {
     useQueryString: true
   }
@@ -18,5 +43,23 @@ request(options, function (error, response, body) {
     console.log(error)
   };
   const obj = JSON.parse(body)
-  console.log(obj)
+  console.log(obj[0].bookmakers[3].markets[0].outcomes)
 });
+
+/*
+
+request(options, function (error, response, body) {
+	if (error) {
+    console.log(error)
+  };
+  const obj = JSON.parse(body)
+  console.log(obj[4].bookmakers[10].markets[1])
+  let updated = obj[4].bookmakers[10].markets[1].outcomes
+  updated.forEach(elem => {
+    elem.price = oddsToPercent(elem.price)
+  })
+  console.log(updated)
+  //obj[0].bookmakers[6].markets[1].outcomes.forEach(elem => console.log(elem.price))
+});
+
+*/
